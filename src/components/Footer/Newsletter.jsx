7221fcfa05933d754a4b512dfe84e231673f81d1
MarwanOladele <oladelemarwan@gmail.com>
2023-01-10 15:08:57 +0100

    my first commit

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import './Newsletter.css';
+import "./Newsletter.css";
+import SubHeading  from "../SubHeading/SubHeading";
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe to Our Newsletter</h1>
+      <p className="p__opensans">And never miss latest Updates!</p>
+      <div className="app__newsletter-input flex__center">
+        <input type="email" placeholder="Email Address" />
+        <button className="custom__button">Subscribe</button>
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+import { FooterOverlay, Newsletter } from "../../components";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 
-import './Footer.css';
+import "./Footer.css";
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
   </div>
 );
 
